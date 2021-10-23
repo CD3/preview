58d5d8130557a1054ca1355491a7bb0b6727475a
CD Clark III <clifton.clark@gmail.com>
2021-10-23 14:20:22 -0500

    added check for sexpect to the installer.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+INSTALLDIR=""
 PREVIEW=$(which preview 2>/dev/null)
 if [[ -n $PREVIEW ]]
 then
@@ -10,7 +11,7 @@ then
         then
                 exit 0
         fi
-
+        INSTALLDIR=$(dirname $PREVIEW)
 fi
 
 declare -a CANDIDATES
@@ -31,13 +32,15 @@ unset IFS
 echo "Found the following (writable) directories in your PATH."
 echo "If you would like to install to a different directory, select `cancel`, add it to your PATH, and rerun."
 PS3="Which directory would you like to install into? "
-select item in "cancel" "${SORTED_CANDIDATES[@]}"
+select item in "$INSTALLDIR" "cancel" "${SORTED_CANDIDATES[@]}"
 do
         if [[ $item == "cancel" ]]
         then
                 echo "Exiting"
                  exit 1
         fi
+
+        [[ -z $item ]] && [[ -z $REPLY ]] && item=$INSTALLDIR
 
         [[ -z $item ]] && item="$REPLY"
         
@@ -57,12 +60,20 @@ done
 INSTALLDIR="$item"
 
 
-echo "Installing 'preview' into '$INSTALLDIR'"
 
+echo "Installing 'preview' into '$INSTALLDIR'"
 if [[ -e preview ]]
 then
         install -m 0755 preview "$INSTALLDIR"
 fi
+
+if [[ ! -d "$HOME/.preview" ]]
+then
+        echo "Creating $HOME/.preview directory."
+        install -d "$HOME/.preview"
+        install -m 0644 dotpreview/* -t "$HOME/.preview"
+fi
+
 
 
 
@@ -90,6 +101,28 @@ then
                 install -m 0755 bin/entr $INSTALLDIR
                 cd "$saved_dir"
                 echo "Installed 'entr' into '$INSTALLDIR'."
+        fi
+fi
+
+if ! which sexpect > /dev/null 2>&1
+then
+        echo "The 'sexpect' command is useful for running interactive viewers in 'preview'. Would you like to install it?"
+        read -p "Y/n: " -n 1 -r answer
+        [[ -z $answer ]] && answer="Y"
+        if [[ $answer =~ ^[yY]$ ]]
+        then
+                echo
+                tmpdir=$(mktemp -d)
+                saved_dir="$PWD"
+                cd $tmpdir
+                echo "Downloading 'sexpect' source to '$tmpdir'."
+                wget  https://github.com/clarkwang/sexpect/archive/refs/tags/v2.3.7.tar.gz > /dev/null 2>&1
+                tar -xzf v2.3.7.tar.gz
+                cmake sexpect-2.3.7 -D CMAKE_INSTALL_PREFIX=./install
+                cmake --build .
+                install -m 0755 sexpect $INSTALLDIR
+                cd "$saved_dir"
+                echo "Installed 'sexpect' into '$INSTALLDIR'."
         fi
 fi
 
